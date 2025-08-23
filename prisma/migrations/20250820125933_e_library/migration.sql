f1c53d5b57f8ece25c9438f8f24dc97a55d83254
Dileep Nishad <crazycodedev143@gmail.com>
2025-08-23 15:21:46 +0530

    Fix errors

--- prisma/migrations/20250820125933_e_library/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."case_laws" ADD COLUMN     "TaxSection" "public"."TaxSection";
-
--- CreateIndex
-CREATE INDEX "case_laws_TaxSection_idx" ON "public"."case_laws"("TaxSection");
-
--- CreateIndex
-CREATE INDEX "case_laws_taxSection_idx" ON "public"."case_laws"("TaxSection");
